17eaadf29ba5f438da6b312ab8bfa560e57d527f
snapzero <lbrandon.blair@gmail.com>
2025-02-07 11:17:45 -0500

    Updated components to loop over data and render accordingly

--- src/Specials.js
+++ src/Specials.js
@@ -4,6 +4,30 @@ import GreekSalad from './assets/greek-salad.jpg';
 import Bruchetta from './assets/bruchetta.svg';
 import LemonDessert from './assets/lemon-dessert.jpg';
 
+const specials = [
+  {
+    id: 1,
+    dish: 'Greek Salad',
+    price: '13.00',
+    image: GreekSalad,
+    description: 'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.'
+  },
+  {
+    id: 2,
+    dish: 'Bruchetta',
+    price: '15.00',
+    image: Bruchetta,
+    description: 'Our delicious Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil to entice you to savor every bite.'
+  },
+  {
+    id: 3,
+    dish: 'Lemon Dessert',
+    price: '9.00',
+    image: LemonDessert,
+    description: 'This comes straight from Grandma&apos;s recipe book, every last ingredient has been sourced and is as authentic as can be imagined with love from the heart.'
+  }
+]
+
 function Specials() {
     return (
       <div className="specials">
@@ -12,15 +36,16 @@ function Specials() {
           <Button type="button" text="Online menu" variant="primary" />
         </section>
         <section className="grid-container">
-          <SpecialCard className="grid-item" title="Greek Salad" price="13.00" image={GreekSalad}>
-            The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.
-          </SpecialCard>
-          <SpecialCard className="grid-item" title="Bruchetta" price="15.00" image={Bruchetta}>
-            Our delicious Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil to entice you to savor every bite.
-          </SpecialCard>
-          <SpecialCard className="grid-item" title="Lemon Dessert" price="9.00" image={LemonDessert}>
-            This comes straight from Grandma&apos;s recipe book, every last ingredient has been sourced and is as authentic as can be imagined with love from the heart.
-          </SpecialCard>
+          {specials.map(special => (
+            <SpecialCard 
+              className="grid-item"
+              key={special.id}
+              title={special.dish}
+              price={special.price}
+              image={special.image}>
+                {special.description}
+            </SpecialCard>
+          ))}
         </section>
       </div>
     );
--- src/Testimonials.js
+++ src/Testimonials.js
@@ -4,23 +4,51 @@ import Person2 from './assets/person2.jpeg';
 import Person3 from './assets/person3.jpg';
 import Person4 from './assets/person4.jpeg';
 
+const reviews = [
+  {
+    id: 1,
+    rating: 5,
+    name: 'Vincent',
+    image: Person1,
+    description: 'Delicous food and excellent service!'
+  },
+  {
+    id: 2,
+    rating: 4,
+    name: 'Angela',
+    image: Person2,
+    description: 'Good food and atmosphere.'
+  },
+  {
+    id: 3,
+    rating: 5,
+    name: 'Kumar',
+    image: Person3,
+    description: 'Great dining experience!'
+  },
+  {
+    id: 4,
+    rating: 3,
+    name: 'Mallory',
+    image: Person4,
+    description: 'Slow service but great food.'
+  },
+]
+
 function Testimonials() {
     return (
       <div className="testimonials" id="testimonials">
         <h2 className="text-center white">Testimonials</h2>
         <section className="grid-container gc-col4">
-          <TestimonialCard rating="5" name="Vincent" image={Person1}>
-            Delicous food and excellent service!
-          </TestimonialCard>
-          <TestimonialCard rating="4" name="Angela" image={Person2}>
-            Good food and atmosphere.
-          </TestimonialCard>
-          <TestimonialCard rating="5" name="Kumar" image={Person3}>
-            Great dining experience!
-          </TestimonialCard>
-          <TestimonialCard rating="3" name="Mallory" image={Person4}>
-            Slow service but great food.
-          </TestimonialCard>
+          {reviews.map(review => (
+            <TestimonialCard
+              key={review.id}
+              rating={review.rating}
+              name={review.name}
+              image={review.image}>
+                {review.description}
+            </TestimonialCard>
+          ))}
         </section>
       </div>
     );
